fd7f6b7225c159f538204cf470524116d3d0e0f0
Eric Wu <ericwu@Erics-MacBook-Pro.local>
2014-04-25 09:28:53 -0700

    firebase sync

--- main-work.js
+++ main-work.js
@@ -1,18 +1,62 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('GameController', function ($scope) {
-	$scope.cells = ['', '', '', '', '', '', '', '', ''];
-	$scope.isXTurn = true;
-	$scope.moves = 9;
-	$scope.anyWinner = false;
+var myApp = angular.module('myApp', ['firebase']);
+myApp.controller('GameController', function ($scope, $firebase) {
+	var tttRef = new Firebase("https://room1super.firebaseio.com/games");
+	
+	// $scope.cells = ['', '', '', '', '', '', '', '', ''];
+	
+	
+	
+	
+
+	tttRef.once("value", function(data){
+		var lastGame;
+		var games = data.val();
+		console.log(data.val());
+		
+		//Get the real objects out of angularified blob
+		if (games != null) 
+		{
+		 	var keys = Object.keys(games); //Get all the screwy text keys
+		    var lastKey = keys[keys.length-1]; //Find the last key
+		    lastGame = games[lastKey]; //Use the last key to get the last game object
+		    if (lastGame.waiting==true) 
+		    {
+		  		//Currently someone is waiting to play
+		 		//Find the Angular version of this game
+		 		lastGame = tttRef.child(lastKey);
+		// 		//whose turn = 1 for player 1, 2 for player 2, 3 for player 1 won, 4 for player 2 won, and 5 for a draw
+		 		lastGame.set ({moves: 9,isXTurn: true, anyWinner: false, waiting: false, cells: ['', '', '', '', '', '', '', '', '']} );
+	 			// player1 = false;
+			} 
+			else 
+			{
+		// 		//This is like when someone opened the page and wanted to start playing
+				lastGame = tttRef.push( {waiting: true} );
+		 		// player1 = true;
+		    }
+		//we have no game
+		}
+		 else 
+		 {
+		  //This is like when someone opened the page and wanted to start playing
+		  lastGame = tttRef.push ({waiting:true});
+		  // player1 = true;
+		}
+
+		$scope.game = $firebase(lastGame);
+		
+	});
+
 	$scope.clickPlay = function (Cellindex)
 			{
-				if($scope.cells[Cellindex] === '')
-					{
-					$scope.cells[Cellindex] = $scope.isXTurn?'X':'O';
-					$scope.moves--;
-					$scope.isXTurn = !$scope.isXTurn;
+				if($scope.game.cells[Cellindex] === '')
+				{
+					$scope.game.cells[Cellindex] = $scope.game.isXTurn?'X':'O';
+					$scope.game.moves--;
+					$scope.game.isXTurn = !$scope.game.isXTurn;
 					$scope.checkWin();
-					}
+					$scope.game.$save();
+				}
 			};
 	$scope.winColumn = [
 							[0, 1, 2],
@@ -26,27 +70,27 @@ myApp.controller('GameController', function ($scope) {
 						];
 
 		$scope.checkWin = function () 
+		{
+			for(var i=0; i < $scope.winColumn.length; i++)
 			{
-				for(var i=0; i < $scope.winColumn.length; i++)
-				{
-					if($scope.cells[$scope.winColumn[i][0]] == $scope.cells[$scope.winColumn[i][1]] &&
-					   $scope.cells[$scope.winColumn[i][0]] == $scope.cells[$scope.winColumn[i][2]])				
-					{	
-						$scope.anyWinner = true;
-						if(	$scope.cells[$scope.winColumn[i][0]] == 'X')
-						{
-							console.log("playerX win");
-						}
-						else if($scope.cells[$scope.winColumn[i][0]] == 'Y')
-						{
-							console.log("playerO win");
-						}
+				if($scope.game.cells[$scope.winColumn[i][0]] == $scope.game.cells[$scope.winColumn[i][1]] &&
+				   $scope.game.cells[$scope.winColumn[i][0]] == $scope.game.cells[$scope.winColumn[i][2]])				
+				{	
+					$scope.game.anyWinner = true;
+					if(	$scope.game.cells[$scope.winColumn[i][0]] == 'X')
+					{
+						console.log("playerX win");
 					}
-					else if ($scope.moves==0 && $scope.anyWinner==false)
-						{
-							console.log("Tie Game");
-						}
+					else if($scope.game.cells[$scope.winColumn[i][0]] == 'Y')
+					{
+						console.log("playerO win");
+					}
 				}
-			};
+				else if ($scope.game.moves==0 && $scope.game.anyWinner==false)
+					{
+						console.log("Tie Game");
+					}
+			}
+		};
 
 });
